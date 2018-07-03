1ba4bd980dfb0efb4b05e7f13a6af94fd9934cce
ericgiannini <eric.giannini@yahoo.com>
2018-07-03 13:33:14 -0700

    Adding project

---
+++ examples/enable-audio/enable-audio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  enable-audio
+//
+//  Created by Eric Giannini on 7/3/18.
+//  Copyright © 2018 Nexmo, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
