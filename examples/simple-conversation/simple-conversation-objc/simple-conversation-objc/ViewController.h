f95398c7e681b928d1280e36faf7253ab564616d
Tony Hung <tbass134@gmail.com>
2018-10-05 13:05:55 -0400

    added ViewController to simple-conversation-objc example

---
+++ examples/simple-conversation/simple-conversation-objc/simple-conversation-objc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  simple-conversation-objc
+//
+//  Created by Tony Hung on 10/5/18.
+//  Copyright © 2018 Nexmo, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
